9f7329a758fec0f5f32eb850f88c9d1d5b144bc2
GogoIsPrograming <76689341+GogoIsPrograming@users.noreply.github.com>
2025-04-13 16:57:59 +0200

    added links and improved spacing a bit

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import DenominationsDisplay from "@/components/denominationsDisplay";
@@ -9,6 +8,7 @@ import {
   calculateDenominations,
   totalDenominations,
 } from "@/components/util/amountNeededCalc";
+import { Send, Github, Globe } from "lucide-react";
 
 export default function Home() {
   const [errorMessage, setErrorMessage] = useState<string>("");
@@ -25,14 +25,17 @@ export default function Home() {
       return;
     }
 
-    const isValid = /^\d+(\.\d{1,2})?$/.test(input);
+    // weil Deutsch spaS macht
+    const formattedInput = input.replace(",", ".");
+
+    const isValid = /^\d+(\.\d{1,2})?$/.test(formattedInput);
     if (!isValid) {
       setErrorMessage("Bitte geben Sie einen gültigen Betrag ein.");
       setDenominations(null);
       return;
     }
 
-    const amount = parseFloat(input);
+    const amount = parseFloat(formattedInput);
     if (amount >= 20000) {
       setErrorMessage(
         "Darf ich fragen, warum Sie so eine große Summe bar bei Ihnen haben?"
@@ -46,7 +49,7 @@ export default function Home() {
 
   const totalAmount = totalDenominations(denominations || {});
   return (
-    <div className="mx-auto grid overflow-x-hidden min-h-screen max-w-lg  grid-rows-[20px_1fr_20px] items-center justify-items-center  pb-20 font-[family-name:var(--font-geist-sans)] shadow-xl shadow-black/20">
+    <div className="mx-auto mt-12 flex flex-col justify-center overflow-x-hidden min-h-screen max-w-lg   font-[family-name:var(--font-geist-sans)] shadow-xl shadow-black/20">
       <main className=" w-full row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
         <div className="px-20 space-y-4 mx-auto">
           <h1 className="text-2xl lg:text-3xl">Euro-Wechselgeld-Rechner</h1>
@@ -66,51 +69,33 @@ export default function Home() {
         </div>
         <DenominationsDisplay denominationsNeeded={denominations} />
       </main>
-      <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
+      <footer className="my-12 text-center px-6 flex flex-wrap items-center justify-center gap-4">
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="flex items-center gap-1 hover:underline hover:underline-offset-4"
+          href="mailto:housein.aboshaar@gmail.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
+          <Send size={16} />
+          E-Mail
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="flex items-center gap-1 hover:underline hover:underline-offset-4"
+          href=""
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
+          <Github size={16} />
+          GitHub Repo
         </a>
         <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          className="flex items-center gap-1 flex-wrap hover:underline hover:underline-offset-4"
+          href="https://www.houseinaboshaar.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          <Globe size={16} />
+          Meine Webseite
         </a>
       </footer>
     </div>
